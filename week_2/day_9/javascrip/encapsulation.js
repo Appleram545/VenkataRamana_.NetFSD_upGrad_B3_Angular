6ae67431765b72af00e439359d523545f7cde547
Ram <appleramana3@gmail.com>
2026-02-26 17:09:12 +0530

    Day 9

---
+++ week_2/day_9/javascrip/encapsulation.js
@@ -0,0 +1,38 @@
+
+class Student {
+
+  constructor(name, rollNumber, marks) {
+    this.name = name;
+    this.rollNumber = rollNumber;
+    this.marks = marks;
+  }
+
+  getDetails() {
+    console.log("Name: " + this.name);
+    console.log("Roll No: " + this.rollNumber);
+    console.log("Marks: " + this.marks);
+  }
+
+  getGrade() {
+    if (this.marks >= 90) {
+      return "A";
+    } else if (this.marks >= 70) {
+      return "B";
+    } else if (this.marks >= 55) {
+      return "C";
+    } else {
+      return "Fail";
+    }
+  }
+}
+
+
+let s1 = new Student("Ravi", 101, 92);
+let s2 = new Student("Anita", 102, 68);
+
+
+s1.getDetails();
+console.log("Grade: " + s1.getGrade());
+
+s2.getDetails();
+console.log("Grade: " + s2.getGrade());
